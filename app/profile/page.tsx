9b321dfcb3be5cb9f92164eb8588866d8a01cbf5
Alan <alan.tressin@outlook.com>
2025-02-20 21:44:15 +0100

    added country selection

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import ProtectedRoute from "../components/ProtectedRoute"
 import { authFetch } from "@/lib/api"
 
@@ -13,6 +13,14 @@ interface UserProfile {
     region: string;
 }
 
+interface Countries {
+    name: string;
+    iso3: string;
+    region: string;
+    subregion: string;
+    emojiU: string;
+}
+
 const ProfilePage = () => {
     const [profile, setProfile] = useState<UserProfile | null>(null)
     const [email, setEmail] = useState('')
@@ -23,6 +31,8 @@ const ProfilePage = () => {
     const [error, setError] = useState('')
     const [success, setSuccess] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+
+    const [countries, setCountries] = useState<Countries[] | []>([])
 
     const fetchProfile = async () => {
         try {
@@ -44,8 +54,26 @@ const ProfilePage = () => {
         }
     }
 
+    const fetchCountries = async () => {
+        try {
+            const res = await authFetch("http://localhost:8080/user/countries?fields=name,iso3,emoji,region,subregion")
+            if (res.status !== 200) {
+                const err = await res.json();
+                setError(err.message || "Failed to load countries");
+                return;
+            }
+            const data = await res.json();
+            setCountries(data)
+
+        } catch (err) {
+            console.error(err)
+            setError("Failed to load countries.")
+        }
+    }
+
     useEffect(() => {
         fetchProfile()
+        fetchCountries()
     }, [])
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -71,6 +99,19 @@ const ProfilePage = () => {
         }
     }
 
+    const handleCountryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selected = e.target.value;
+        setCountry(selected);
+
+        //@ts-ignore
+        const selectedCountry = countries.find((c) => c.name === selected);
+        if (selectedCountry) {
+            setRegion(selectedCountry.subregion);
+        } else {
+            setRegion("");
+        }
+    }
+
     return (
         <ProtectedRoute>
             <div>
@@ -79,9 +120,12 @@ const ProfilePage = () => {
                 {success && <p className="text-green-500">{success}</p>}
                 {profile ? (
                     <form onSubmit={handleSubmit} className="space-y-4">
+                        <div id={"profile-image-div"} className={"rounded mx-auto flex justify-center"}>
+                            <img src={"/profile-pic-empty.svg"} alt={"profile-image"} className={"max-w-48 min-h-48"} />
+                        </div>
                         <div>
                             <label className="block">Username</label>
-                            <input type="text" value={profile.username} readOnly className="border p-2 w-full bg-gray-200" />
+                            <input type="text" value={profile.username} readOnly className="border p-2 w-full bg-gray-800 rounded" />
                         </div>
                         <div>
                             <label className="block">Email</label>
@@ -89,18 +133,28 @@ const ProfilePage = () => {
                                 type="email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
-                                className="border p-2 w-full"
+                                className="border p-2 w-full rounded"
                                 required
                             />
                         </div>
                         <div>
                             <label className="block">Country</label>
-                            <input
-                                type="text"
-                                value={country}
-                                onChange={(e) => setCountry(e.target.value)}
-                                className="border p-2 w-full"
-                            />
+                            {countries && countries.length > 0 ? (
+                                <select
+                                    value={country}
+                                    onChange={handleCountryChange}
+                                    className="border p-2 w-full rounded"
+                                >
+                                    <option value="">Select a country</option>
+                                    {countries.map((c) => (
+                                        <option key={c.iso3} value={c.name}>
+                                            {c.name}
+                                        </option>
+                                    ))}
+                                </select>
+                            ) : (
+                                <p>Loading countries...</p>
+                            )}
                         </div>
                         <div>
                             <label className="block">Age</label>
@@ -108,25 +162,29 @@ const ProfilePage = () => {
                                 type="number"
                                 value={age}
                                 onChange={(e) => setAge(Number(e.target.value))}
-                                className="border p-2 w-full"
+                                className="border p-2 w-full rounded"
                             />
                         </div>
                         <div>
                             <label className="block">Gender</label>
-                            <input
-                                type="text"
+                            <select
                                 value={gender}
                                 onChange={(e) => setGender(e.target.value)}
-                                className="border p-2 w-full"
-                            />
+                                className="border p-2 w-full rounded"
+                            >
+                                <option value={"m"}>Male</option>
+                                <option value={"f"}>Female</option>
+                                <option value={"d"}>Diverse</option>
+                            </select>
                         </div>
                         <div>
                             <label className="block">Region</label>
                             <input
                                 type="text"
                                 value={region}
+                                readOnly
                                 onChange={(e) => setRegion(e.target.value)}
-                                className="border p-2 w-full"
+                                className="border p-2 w-full bg-gray-800 rounded"
                             />
                         </div>
                         <button
