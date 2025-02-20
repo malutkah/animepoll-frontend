d7e79ee0f1f1403ecf303fb42b924a16a1e04ded
Alan <alan.tressin@outlook.com>
2025-02-20 21:42:31 +0100

    added icons and link to /discover

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import ThemeToggle from "./ThemeToggle"
+import { Home, Search, User, LogOut, LogIn, UserPlus } from "lucide-react"
 
 const Navbar = () => {
     const [loggedIn, setLoggedIn] = useState(false)
@@ -18,53 +19,66 @@ const Navbar = () => {
     }, [])
 
     const handleLogout = () => {
-        // Clear token and related data then redirect to landing page
         localStorage.removeItem("token")
         localStorage.removeItem("expires_at")
         router.push("/")
+        setLoggedIn(false)
     }
 
     return (
         <nav className="p-4 flex justify-between items-center bg-white dark:bg-gray-800 shadow-md relative">
-            <Link href="/" className="text-2xl font-bold text-indigo-600 dark:text-indigo-400">
-                AnimePoll
+            <Link href="/" className="flex items-center space-x-2 text-2xl font-bold text-indigo-600 dark:text-indigo-400">
+                <Home className="h-6 w-6" />
+                <span>AnimePoll</span>
             </Link>
             {/* Desktop Navigation */}
-            <div className="hidden md:flex items-center space-x-4">
+            <div className="hidden md:flex items-center space-x-6">
                 {loggedIn ? (
                     <>
                         <Link
-                            href="/dashboard"
-                            className="text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
+                            href="/discover"
+                            className="flex items-center space-x-1 text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
                         >
-                            Dashboard
+                            <Search className="h-5 w-5" />
+                            <span>Discover Polls</span>
+                        </Link>
+                        <Link
+                            href="/dashboard"
+                            className="flex items-center space-x-1 text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
+                        >
+                            <Home className="h-5 w-5" />
+                            <span>Dashboard</span>
                         </Link>
                         <Link
                             href="/profile"
-                            className="text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
+                            className="flex items-center space-x-1 text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
                         >
-                            Profile
+                            <User className="h-5 w-5" />
+                            <span>Profile</span>
                         </Link>
                         <button
                             onClick={handleLogout}
-                            className="text-gray-600 dark:text-gray-300 hover:text-red-600 dark:hover:text-red-400"
+                            className="flex items-center space-x-1 text-red-400 "
                         >
-                            Logout
+                            <LogOut className="h-5 w-5" />
+                            <span>Logout</span>
                         </button>
                     </>
                 ) : (
                     <>
                         <Link
                             href="/login"
-                            className="text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
+                            className="flex items-center space-x-1 text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
                         >
-                            Login
+                            <LogIn className="h-5 w-5" />
+                            <span>Login</span>
                         </Link>
                         <Link
                             href="/signup"
-                            className="text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
+                            className="flex items-center space-x-1 text-gray-600 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400"
                         >
-                            Sign Up
+                            <UserPlus className="h-5 w-5" />
+                            <span>Sign Up</span>
                         </Link>
                     </>
                 )}
@@ -94,43 +108,56 @@ const Navbar = () => {
                             <>
                                 <Link
                                     href="/dashboard"
-                                    className="block mb-2 text-gray-600 dark:text-gray-300 hover:text-indigo-600"
+                                    className="flex items-center space-x-1 block mb-2 text-gray-600 dark:text-gray-300 hover:text-indigo-600"
                                     onClick={() => setMenuOpen(false)}
                                 >
-                                    Dashboard
+                                    <Home className="h-5 w-5" />
+                                    <span>Dashboard</span>
+                                </Link>
+                                <Link
+                                    href="/discover"
+                                    className="flex items-center space-x-1 block mb-2 text-gray-600 dark:text-gray-300 hover:text-indigo-600"
+                                    onClick={() => setMenuOpen(false)}
+                                >
+                                    <Search className="h-5 w-5" />
+                                    <span>Discover Polls</span>
                                 </Link>
                                 <Link
                                     href="/profile"
-                                    className="block mb-2 text-gray-600 dark:text-gray-300 hover:text-indigo-600"
+                                    className="flex items-center space-x-1 block mb-2 text-gray-600 dark:text-gray-300 hover:text-indigo-600"
                                     onClick={() => setMenuOpen(false)}
                                 >
-                                    Profile
+                                    <User className="h-5 w-5" />
+                                    <span>Profile</span>
                                 </Link>
                                 <button
                                     onClick={() => {
                                         handleLogout()
                                         setMenuOpen(false)
                                     }}
-                                    className="block text-gray-600 dark:text-gray-300 hover:text-red-600"
+                                    className="flex items-center space-x-1 dark:text-gray-300 text-red-600"
                                 >
-                                    Logout
+                                    <LogOut className="h-5 w-5" />
+                                    <span>Logout</span>
                                 </button>
                             </>
                         ) : (
                             <>
                                 <Link
                                     href="/login"
-                                    className="block mb-2 text-gray-600 dark:text-gray-300 hover:text-indigo-600"
+                                    className="flex items-center space-x-1 block mb-2 text-gray-600 dark:text-gray-300 hover:text-indigo-600"
                                     onClick={() => setMenuOpen(false)}
                                 >
-                                    Login
+                                    <LogIn className="h-5 w-5" />
+                                    <span>Login</span>
                                 </Link>
                                 <Link
                                     href="/signup"
-                                    className="block text-gray-600 dark:text-gray-300 hover:text-indigo-600"
+                                    className="flex items-center space-x-1 block text-gray-600 dark:text-gray-300 hover:text-indigo-600"
                                     onClick={() => setMenuOpen(false)}
                                 >
-                                    Sign Up
+                                    <UserPlus className="h-5 w-5" />
+                                    <span>Sign Up</span>
                                 </Link>
                             </>
                         )}
