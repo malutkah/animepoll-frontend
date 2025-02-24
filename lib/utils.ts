b2b2e52e7261e3a8dacd69aa69e35342c236a819
Alan <alan.tressin@outlook.com>
2025-02-24 14:28:50 +0100

    exported formatTimestamp function to utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const formatTimestamp = (s: string): string => {
+  const date = new Date(s);
+  const day = date.getUTCDate().toString().padStart(2, "0");
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const year = date.getUTCFullYear();
+  const hours = date.getUTCHours().toString().padStart(2, "0");
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+  return `${day}.${month}.${year}  ${hours}:${minutes}`;
+};
