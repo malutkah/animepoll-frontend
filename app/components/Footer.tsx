6ff3a30c35d6b90b18f43d6100fb4203f1021150
Alan <alan.tressin@outlook.com>
2025-02-27 12:23:38 +0100

    added footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+
+const Navbar = () => {
+    return (
+        <div className="p-4 flex justify-start gap-8 items-center bg-white dark:bg-gray-800 shadow-md">
+            <Link
+                href="/legal"
+                className="flex items-center space-x-1 text-gray-600 dark:text-gray-300 dark:hover:text-sky-500"
+            > Legal </Link>
+
+            <Link
+                href="/imprint"
+                className="flex items-center space-x-1 text-gray-600 dark:text-gray-300 dark:hover:text-sky-500"
+            > Imprint </Link>
+        </div>
+    )
+}
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import "./globals.css"
-import { Inter } from "next/font/google"
+import { Inter, Montserrat } from "next/font/google"
 import { ThemeProvider } from "next-themes"
 import Navbar from "@/app/components/Navbar"
 import { ToastProvider } from "@/app/components/ToastProvider"
 import MaintenanceGate from "@/app/components/MaintenanceGate"
+import Footer from "@/app/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] })
+const mon = Montserrat({ weight: "400", subsets: ["latin"] })
 
 export const metadata = {
     title: "AnimePoll",
@@ -15,13 +17,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-        <body className={inter.className}>
+        <body className={mon.className}>
         <ThemeProvider attribute="class">
             <ToastProvider>
                 <MaintenanceGate>
-                    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+                    <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 transition-colors duration-300">
                         <Navbar />
-                        <main className="container mx-auto px-4 py-8">{children}</main>
+                        <main className="container mx-auto px-4 py-8 flex-1">
+                            {children}
+                        </main>
+                        <Footer />
                     </div>
                 </MaintenanceGate>
             </ToastProvider>
