ef67ca0ec44d426a6d9e8063e7fe041988f40d46
Alan <alan.tressin@outlook.com>
2025-02-27 12:23:49 +0100

    added legal and imprint page

---
+++ app/imprint/page.tsx
@@ -0,0 +1,18 @@
+
+
+const Imprint = () =>  {
+    return (
+        <div className={"block"}>
+            <h1 className="text-3xl font-bold mb-4">Impressum</h1>
+            <p>Alan Tressin</p>
+            <p>c/o MDC Management#689</p>
+            <p>Welserstraße 3</p>
+            <p className={"mb-6"}>87463 Dietmannsried</p>
+            <h3 className="text-xl font-semibold mt-4 mb-2">Kontakt</h3>
+            <p>E-Mail: contact@animepoll.net</p>
+        </div>
+
+    )
+}
+
+export default Imprint;
