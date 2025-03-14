beadcdd95df94da723cc222c7b9bd528e897f2c2
Alan <alan.tressin@outlook.com>
2025-03-14 01:50:38 +0100

    check if token param is valid and redirect

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import {baseURL} from "@/lib/api";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
     const maintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true'
     const pathname = req.nextUrl.pathname
 
@@ -9,6 +10,26 @@ export function middleware(req: NextRequest) {
     if (maintenanceMode && pathname !== '/') {
         return NextResponse.redirect(new URL('/', req.url))
     }
+
+    // check if toke param is valid
+    if (pathname === '/password-reset') {
+        const params = req.nextUrl.searchParams.get("token");
+        if (!params || params === "") {
+            return NextResponse.redirect(new URL('/not-found', req.url))
+        }
+
+        const base = req.url.startsWith("https://animepoll") ? "https://api.animepoll.net" : "http://localhost:8080"
+
+        const res = await fetch(base + "/user/check-reset-token?token="+params)
+
+        if (res.status !== 200) {
+            return NextResponse.redirect(new URL('/not-found', req.url))
+        }
+
+        // return NextResponse.redirect(new URL('/not-found', req.url))
+
+    }
+
     return NextResponse.next()
 }
 
